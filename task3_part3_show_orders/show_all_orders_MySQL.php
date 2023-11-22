8900f55276d92853494caec8582830cbf2eef442
mohammadhosien <rastegarmohammadhosien@gmail.com>
2023-11-22 20:13:35 +0330

    successfull result

--- task3_part3_show_orders/show_all_orders_MySQL.php
+++ task3_part3_show_orders/show_all_orders_MySQL.php
@@ -12,11 +12,11 @@ $password = "";
 try {
     $database = new PDO("mysql:host=$servername;dbname=$database_name", $username, $password,);
 
-    $productName = "";
+    
     //user_id , user_name , product_id دسترسی به موارد
     foreach ($database->query("SELECT * FROM users RIGHT JOIN orders ON users.user_id = orders.user_id")->fetchAll(PDO::FETCH_OBJ) as $user) {
         $user = (array)$user;
-
+        $productName = "";
         // به یک لیست برای دستیابی به نام محصولات product_id تبدیل استرینگ درون
         $productIds = explode(" , ", $user["product_id"]);
         // product_id یک حلقه پیمایش برای تمام آی دی های محصولات ثبت شده درون 
@@ -28,12 +28,14 @@ try {
                 if ((int)$productId == $value["product_id"]) {
                     $productName .= $value["product_name"] . " , ";
                     break;
+                } elseif ((int)$productId == 0) {
+                    break;
                 }
             }
         }
         // order_products ارسال اطلاعات به جدول
         $query = 'INSERT INTO order_products (user_id,user_name, product_id, product_name)
-        VALUES(?,?,?,?) ';
+    VALUES(?,?,?,?) ';
         //یک سند برای ارسال به دیتابیس آماده میکند
         $stml = $database->prepare($query);
 
@@ -41,16 +43,14 @@ try {
         $stml->execute([$user['user_id'], $user['user_name'], $user['product_id'], $productName]);
         //اتمام فرایند ارسال اطلاعات
 
-        //شروع دریافت اطلاعات از جدول برای نمایش به کاربر
-        $query = 'SELECT order_products (user_id,user_name, product_id, product_name)
-        VALUES(?,?,?,?) ';
-        //یک سند برای ارسال به دیتابیس آماده میکند
-        $stml = $database->prepare($query);
-
-        //مقادیر سند ایجاد شده را تنظیم کرده و به دیتابیس ارسال میکند
-        $stml->execute([$user['user_id'], $user['user_name'], $user['product_id'], $productName]);
-        //اتمام فرایند ارسال اطلاعات
-
+        
+    }
+    //شروع دریافت اطلاعات از جدول برای نمایش به کاربر
+    $all_orders = [];
+    foreach ($database->query("SELECT * FROM order_products ")->fetchAll(PDO::FETCH_OBJ)  as $orders) {
+        $orders = (array)$orders;
+        $all_orders[] = $orders;
+        
     }
 } catch (PDOException $e) {
     echo $user . "<br>" . $e->getMessage();
