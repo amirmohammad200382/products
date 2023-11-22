d45f793d291fa5be4fa9925bdfa37d1ad28d3863
mohammadhosien <rastegarmohammadhosien@gmail.com>
2023-11-22 20:45:08 +0330

    no important change

--- task3_part3_show_orders/show_all_orders_MySQL.php
+++ task3_part3_show_orders/show_all_orders_MySQL.php
@@ -12,7 +12,7 @@ $password = "";
 try {
     $database = new PDO("mysql:host=$servername;dbname=$database_name", $username, $password,);
 
-    
+
     //user_id , user_name , product_id دسترسی به موارد
     foreach ($database->query("SELECT * FROM users RIGHT JOIN orders ON users.user_id = orders.user_id")->fetchAll(PDO::FETCH_OBJ) as $user) {
         $user = (array)$user;
@@ -33,7 +33,9 @@ try {
                 }
             }
         }
+        
         // order_products ارسال اطلاعات به جدول
+        
         $query = 'INSERT INTO order_products (user_id,user_name, product_id, product_name)
     VALUES(?,?,?,?) ';
         //یک سند برای ارسال به دیتابیس آماده میکند
@@ -42,15 +44,15 @@ try {
         //مقادیر سند ایجاد شده را تنظیم کرده و به دیتابیس ارسال میکند
         $stml->execute([$user['user_id'], $user['user_name'], $user['product_id'], $productName]);
         //اتمام فرایند ارسال اطلاعات
-
+   
         
+       
     }
     //شروع دریافت اطلاعات از جدول برای نمایش به کاربر
     $all_orders = [];
     foreach ($database->query("SELECT * FROM order_products ")->fetchAll(PDO::FETCH_OBJ)  as $orders) {
         $orders = (array)$orders;
         $all_orders[] = $orders;
-        
     }
 } catch (PDOException $e) {
     echo $user . "<br>" . $e->getMessage();
